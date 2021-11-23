24a867166611b396f822914005976b6896dd7248
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2021-11-23 12:57:40 -0800

    added axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,42 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import Search from "./Search";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="container">
-        <div className="weather-app-wrapper">
-          <div className="weather-app">
-            <div className="overview">
-              <h1> San Diego </h1>
-              <h2>
-                <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" />
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-                <span className="temprature"> 24 </span>
-                <span className="unit">
-                  <a href="/" className="active">
-                    °C{" "}
-                  </a>
-                  |{" "}
-                  <a href="/" className="current">
-                    °F
-                  </a>
-                </span>
-              </h2>
-              <ul>
-                <li></li>
-                <span> Cloudy </span>
-                <li>
-                  <span> Friday 10 am</span>
-                </li>
-                <li>Humidity:25%</li>
-                <li>Wind: 2 km/h</li>
-              </ul>
-              <Search />
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: "Friday 10:00",
+      description: response.data.weather[0].description,
+      iconUrl: "http://openweathermap.org/img/wn/50d@2x.png",
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="container">
+          <div className="weather-app-wrapper">
+            <div className="weather-app">
+              <div className="overview">
+                <h1>{weatherData.city} </h1>
+                <h2>
+                  <img
+                    src={weatherData.iconUrl}
+                    alt={weatherData.description}
+                  />
+
+                  <span className="temperature">
+                    {Math.round(weatherData.temperature)}{" "}
+                  </span>
+                  <span className="unit">
+                    <a href="/" className="active">
+                      °C{" "}
+                    </a>
+                    |{" "}
+                    <a href="/" className="current">
+                      °F
+                    </a>
+                  </span>
+                </h2>
+                <ul>
+                  <li></li>
+                  <span className="text-capitalize">
+                    {weatherData.description}{" "}
+                  </span>
+                  <li>
+                    <span>{weatherData.date}</span>
+                  </li>
+                  <li>Humidity: {weatherData.humidity}%</li>
+                  <li>Wind: {weatherData.wind} km/h</li>
+                </ul>
+                <Search />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "6ebe89c8349b2767cc0894daf553186c";
+    let city = "San Diego";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
