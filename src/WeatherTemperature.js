5bfbd5c87f3c9304460e9e0c5bf3293cc104010f
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2021-12-02 17:53:29 -0800

    added WeatherIcon.js

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
   const [unit, setUnit] = useState("celsius");
-  function convertToFarenheit(event) {
+  function convertToFahrenheit(event) {
     event.preventDefault();
     setUnit("fahrenheit");
   }
@@ -12,13 +12,17 @@ export default function WeatherTemperature(props) {
     setUnit("celsius");
   }
 
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+
   if (unit === "celsius") {
     return (
       <div className="WeatherTemperature">
         <span className="temperature">{Math.round(props.celsius)} </span>
         <span className="unit">
           °C |{" "}
-          <a href="/" onClick={convertToFarenheit}>
+          <a href="/" onClick={convertToFahrenheit}>
             °F
           </a>
         </span>
