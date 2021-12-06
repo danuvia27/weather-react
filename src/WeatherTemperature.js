f57d6e960c9eeb67aa5e5c758f91cc7415b8a088
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2021-12-06 11:15:13 -0800

    edit  CSS on weatherinfo.js

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -2,14 +2,18 @@ import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
   const [unit, setUnit] = useState("celsius");
-  function convertToFahrenheit(event) {
+  function showFahrenheit(event) {
     event.preventDefault();
     setUnit("fahrenheit");
   }
 
-  function convertToCelsius(event) {
+  function showCelsius(event) {
     event.preventDefault();
     setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
   }
 
   if (unit === "celsius") {
@@ -18,19 +22,18 @@ export default function WeatherTemperature(props) {
         <span className="temperature">{Math.round(props.celsius)} </span>
         <span className="unit">
           °C |{" "}
-          <a href="/" onClick={convertToFahrenheit}>
+          <a href="/" onClick={showFahrenheit}>
             °F
           </a>
         </span>
       </div>
     );
   } else {
-    let fahrenheit = (props.celsius * 9) / 5 + 32;
     return (
       <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(fahrenheit)} </span>
+        <span className="temperature">{Math.round(fahrenheit())} </span>
         <span className="unit">
-          <a href="/" onClick={convertToCelsius}>
+          <a href="/" onClick={showCelsius}>
             °C
           </a>{" "}
           | °F
