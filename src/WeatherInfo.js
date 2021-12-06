c550cd9fad08e8980e5043c41bccdd96a7a85840
danuvia27 <87292336+danuvia27@users.noreply.github.com>
2021-12-06 11:23:38 -0800

    Updated WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,29 +1,41 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
-import WeatherIcon from "./WeatherTemperature";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <div className="overview">
         <h1>{props.data.city} </h1>
-        <h2>
-          <div className="float-left">
-            <WeatherIcon code={props.data.icon} />
+        <div className="row">
+          <div className="col-11">
+            <div className="clearfix">
+              <div className="float-left">
+                <div class="d-flex justify-content-center ">
+                  <WeatherIcon code={props.data.icon} />
+                  <div className="float-left">
+                    <WeatherTemperature celsius={props.data.temperature} />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <span className="temperature">
-            {Math.round(props.data.temperature)}{" "}
-          </span>
-        </h2>
-
-        <ul>
-          <li className="text-capitalize">{props.data.description} </li>
-          <li>
-            <FormattedDate date={props.data.date} />
-          </li>
-          <li>Humidity: {props.data.humidity}%</li>
-          <li>Wind: {Math.round(props.data.wind)} km/h</li>
-        </ul>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <div class="d-flex justify-content-center">
+            <ul>
+              <li className="text-capitalize">{props.data.description} </li>
+              <li>
+                <FormattedDate date={props.data.date} />
+              </li>
+              <li>Humidity: {props.data.humidity}%</li>
+              <li>Wind: {Math.round(props.data.wind)} km/h</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
